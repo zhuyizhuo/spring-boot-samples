651cf58095de0b50ca75129391f0c61600319a62
zhuyizhuo <zhu.yizhuo@163.com>
2020-06-27 22:40:52 +0800

    :sparkles: 新增 Liquibase java 操作数据库 demo
    :sparkles: 新增 Liquibase java 操作数据库 demo

--- spring-boot-2.x-samples/spring-boot-samples-liquibase/src/main/java/com/github/zhuyizhuo/liquibase/LiquibaseApplication.java
+++ spring-boot-2.x-samples/spring-boot-samples-liquibase/src/main/java/com/github/zhuyizhuo/liquibase/LiquibaseApplication.java
@@ -1,4 +1,4 @@
-package com.github.zhuyizhuo.liquibase.sample;
+package com.github.zhuyizhuo.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ spring-boot-2.x-samples/spring-boot-samples-liquibase/src/main/java/com/github/zhuyizhuo/liquibase/sample/FixUsername.java
@@ -0,0 +1,55 @@
+package com.github.zhuyizhuo.liquibase.sample;
+
+import liquibase.change.custom.CustomTaskChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class FixUsername implements CustomTaskChange {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void execute(Database database) throws CustomChangeException {
+        JdbcConnection connection = (JdbcConnection) database.getConnection();
+        try (PreparedStatement psmt = connection.prepareStatement("SELECT id, username, password, create_time FROM users")) {
+            try (ResultSet rs = psmt.executeQuery()) {
+                while (rs.next()) {
+                    String username = rs.getString("username");
+                    if ("zhuo".equals(username)) {
+                        Integer id = rs.getInt("id");
+                        // todo sth..
+                        logger.info("[migrate][更新 user({}) 的用户名({} => {})", id, username, "new user");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        return null;
+    }
+
+    @Override
+    public void setUp() throws SetupException {
+    }
+
+    @Override
+    public void setFileOpener(ResourceAccessor resourceAccessor) {
+    }
+
+    @Override
+    public ValidationErrors validate(Database database) {
+        return null;
+    }
+}
