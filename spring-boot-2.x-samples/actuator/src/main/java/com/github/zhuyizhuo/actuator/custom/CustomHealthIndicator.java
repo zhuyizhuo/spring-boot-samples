586274df0c92725bacfabab7664bc21910779669
zhuyizhuo <2361883887@qq.com>
2021-10-15 12:51:49 +0800

    新增 actuator 示例
    新增 actuator 示例

---
+++ spring-boot-2.x-samples/actuator/src/main/java/com/github/zhuyizhuo/actuator/custom/CustomHealthIndicator.java
@@ -0,0 +1,17 @@
+package com.github.zhuyizhuo.actuator.custom;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        // 使用 builder 来创建健康状态信息
+        // 如果你throw 了一个 exception，那么status 就会被置为DOWN，异常信息会被记录下来
+        builder.up()
+                .withDetail("app", "项目很健康！")
+                .withDetail("error", "项目有点问题！");
+    }
+}
